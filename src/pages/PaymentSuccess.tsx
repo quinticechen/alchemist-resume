0470cff5be789e9c29dd1b06f801e96f483ccc51
quintice <lovejoker369@gmail.com>
2025-02-21 12:18:44 +0800

    PaymentIssue

--- src/pages/PaymentSuccess.tsx
+++ src/pages/PaymentSuccess.tsx
@@ -1,5 +1,7 @@
 
 import React from 'react';
+import Lottie from 'react-lottie';
+import animationData from "@/animations/Jellyfish.yellow.money.json"; 
 
 const PaymentSuccess: React.FC = () => {
     const handleProceedToWorkspace = () => {
@@ -8,10 +10,9 @@ const PaymentSuccess: React.FC = () => {
 
     return (
         <div style={{ textAlign: 'center', padding: '20px' }}>
-            <video width="100%" controls autoPlay loop>
-                <source src="https://vhofgqmmovjtcnakowlv.supabase.co/storage/v1/object/sign/elements/Jellyfish-Yellow-Money.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJlbGVtZW50cy9KZWxseWZpc2gtWWVsbG93LU1vbmV5Lm1wNCIsImlhdCI6MTc0MDA0NjY2NSwiZXhwIjozMzI0NDUxMDY2NX0.cdnvOQEZc6gF2DLwfLXcN393IPYCuyAG7IUVW1z1jd0" type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+            <div style={{ width: '100%' }}>
+                <Lottie options={defaultOptions} height={400} width={'100%'} />
+            </div>
             <h1 style={{ color: '#6d3666' }}>Payment Success</h1>
             <p style={{ color: '#fec948' }}>Thank you for your subscription! Here are your subscription plan details:</p>
             {/* 在這裡顯示訂閱計劃詳情 */}
