63ec90be22e26d23d59b1d4ccf2dbc29cfb63c14
quintice <lovejoker369@gmail.com>
2025-02-21 12:34:35 +0800

    PaymentIssue

--- supabase/functions/stripe-payment/index.ts
+++ supabase/functions/stripe-payment/index.ts
@@ -5,7 +5,7 @@ import Stripe from "https://esm.sh/stripe@14.18.0?target=deno";
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-application-name',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-application-name, x-environment',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
 };
 
