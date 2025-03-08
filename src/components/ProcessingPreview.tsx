8a8650f7a4753064238f7d32e5328214c3ad88db
quintice <lovejoker369@gmail.com>
2025-03-08 16:37:11 +0800

    FixTimeOut

--- src/components/ProcessingPreview.tsx
+++ src/components/ProcessingPreview.tsx
@@ -11,8 +11,8 @@ import { useNavigate } from "react-router-dom";
 interface ProcessingPreviewProps {
   analysisId?: string;
   jobUrl?: string;
-  // resumeId?: string;
-  isProcessing?:
+  resumeId?: string;
+  isProcessing?: boolean;
   setIsProcessing?: (isProcessing: boolean) => void;
   onGenerationComplete?: () => void;
 }
@@ -22,7 +22,7 @@ type ProcessingStatus = "idle" | "loading" | "error" | "success";
 const ProcessingPreview = ({
   analysisId,
   jobUrl,
-  // resumeId,
+  resumeId,
   isProcessing,
   setIsProcessing,
   onGenerationComplete,
